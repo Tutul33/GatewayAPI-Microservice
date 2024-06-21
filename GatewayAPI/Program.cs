c4e8f36bf1484c54260c8961327584ee506702ad
Tutul Chakma <tutulcou@gmail.com>
2024-06-21 18:16:16 +0600

    Commit

--- GatewayAPI/Program.cs
+++ GatewayAPI/Program.cs
@@ -1,6 +1,23 @@
+using GatewayAPI.Protos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Services.AddGrpcClient<Organization.OrganizationClient>(o =>
+//{
+//    o.Address = new Uri("http://localhost:5249");
+//});
+builder.Services.AddGrpcClient<Organization.OrganizationClient>(o =>
+{
+    o.Address = new Uri("http://localhost:5001");
+}).ConfigureChannel(o =>
+{
+    o.HttpHandler = new SocketsHttpHandler
+    {
+        PooledConnectionLifetime = TimeSpan.FromMinutes(5),
+        EnableMultipleHttp2Connections = true
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
